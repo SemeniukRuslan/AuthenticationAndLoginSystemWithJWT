a5f9d9ac51dd9909633fde04f1c9f482e985e7a9
Ruslan Semeniuk <semeniukruslan@ukr.net>
2023-04-13 10:34:41 +0200

    update AuthEntryPointJWT, JWTUtil

--- src/main/java/com/example/demo/security/jwt/AuthEntryPointJWT.java
+++ src/main/java/com/example/demo/security/jwt/AuthEntryPointJWT.java
@@ -1,6 +1,9 @@
 package com.example.demo.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -8,6 +11,9 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 @Component
 @Slf4j
@@ -16,7 +22,16 @@ public class AuthEntryPointJWT implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        log.error("Unauthorized error: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+
+        final Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+
+        final String bodyJson = new ObjectMapper().writeValueAsString(body);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(bodyJson);
     }
 }
--- src/main/java/com/example/demo/security/jwt/JWTUtil.java
+++ src/main/java/com/example/demo/security/jwt/JWTUtil.java
@@ -1,8 +1,7 @@
 package com.example.demo.security.jwt;
 
 import com.example.demo.security.services.UserDetailsImpl;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -44,8 +43,16 @@ public class JWTUtil {
                     .setSigningKey(jwtSecret)
                     .parseClaimsJws(authToken);
             return true;
-        } catch (Exception e) {
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
             log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
         }
         return false;
     }
